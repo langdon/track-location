ed0c5585479e1bc9fb7ae90edbe059143e5c35cc
libra guest <langdon@redhat.com>
2012-08-05 00:30:50 -0400

    coding

--- onx-client/onx-post-location.js
+++ onx-client/onx-post-location.js
@@ -8,16 +8,40 @@ var targetServer = "http://trackloc-rheldev.rhcloud.com/track-location/";
 
 console.log('Started script: When anyone texts me \"' +  messageText + '\" post with my location');
 
+ device.scheduler.setTimer({
+      name: "sendLocationAlarm", 
+      time: 0,
+      interval: 5*1000, 
+      exact: false },
+      function () { device.notifications.createNotification('Hello world!').show(); });
+
+ device.screen.on('off', function () 
+       { device.scheduler.removeTimer("uniqueAlarmName"); });
+       
+// Initializing variables 
+
+var messageText = "where2?";
+//var targetServer = "http://trackloc-rheldev.rhcloud.com/track-location?geoX=lat&geoY=lon&time=currTime";
+var targetServer = "http://trackloc-rheldev.rhcloud.com/track-location/";
+
+// End of variables initializing 
+
+console.log('Started script: When anyone texts me \"' +  messageText + '\" post with my location');
+
+console.log("it is updated");
+
 //  Register callback on sms received event
 device.messaging.on('smsReceived', function (sms) {
     if (sms.data.body.toLowerCase() === messageText.toLowerCase()) {
+        //console.log('messageText: ' + messageText  + ", sms.data.body.toLowerCase(): " + sms.data.body.toLowerCase() + ", sms.data.from: " + sms.data.from);
         // getting location from cell, which is accurate enough in this case, time interval is 100  milliseconds, to get immediate location sample
         var locListener = device.location.createListener('CELL', 100);
         locListener.on('changed', function (signal) {
             // stop listening to location changed events after getting the current location
             locListener.stop();
-
-            var locationTrackerUrl = targetServer.replace(/lat/g, signal.location.latitude).replace(/lon/g, signal.location.longitude).replace(/currTime/g, d.getTime());
+/*
+            //var locationTrackerUrl = targetServer.replace(/lat/g, signal.location.latitude).replace(/lon/g, signal.location.longitude).replace(/currTime/g, d.getTime());
+            var locationTrackerUrl = targetServer
 
             var d = new Date();
             var notification = device.notifications.createNotification('Calling locationTrackerUrl: ' + locationTrackerUrl);
@@ -26,9 +50,9 @@ device.messaging.on('smsReceived', function (sms) {
                   url: locationTrackerUrl,
                   type: 'POST',
                   data {
-                      'geoX', signal.location.latitude,
-                      'geoY', signal.location.longitude,
-                      'time', d.getTime()
+                      'geoX': "'" + signal.location.latitude + "'",
+                      'geoY': "'" + signal.location.longitude + "'",
+                      'time': "'" + d.getTime() + "'"
                   }
                 },
                 function onSuccess(body, textStatus, response) {
@@ -42,6 +66,7 @@ device.messaging.on('smsReceived', function (sms) {
                   error.statusCode = response.status;
                     console.error('error: ',error);
                 });
+                */
         });
         locListener.start();
     }
